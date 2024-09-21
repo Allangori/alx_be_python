fa89c9ba2fa598278245daed44a8eeba9d02343c
Allan Gori <ogollogori@gmail.com>
2024-09-21 09:39:06 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,7 +5,7 @@ if weather == "sunny" :
     print("Wear a t-shirt and sunglasses.")
 
 elif weather == "rainy" :
-    print("Don't forget your umbrella and rain coat")
+    print("Don't forget your umbrella and a rain coat")
 
 elif weather == "cold" :
     print("Make sure to wear a warm coat and a scarf")
