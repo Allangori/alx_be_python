e65169c356803868a56ce71e6f3f76ec8b7bc6b8
Allan Gori <ogollogori@gmail.com>
2024-09-28 08:58:06 +0300

    neww updates

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,33 +1,16 @@
+x = 3
+
 def perform_operation(num1 , num2, operation):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Error: Division by zero"  # Handling division by zero
-        return num1 / num2
-    else:
-        return "Error: Invalid operation"  # Handling invalid operations
-
-
-
-
-#def perform_operation ( num1, num2, operation):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Error: Division by zero"  
-        return num1 / num2
-    else :
-        print('Choose a function to be performed.')
-
-    
-
+        if operation == 'add':
+            return num1 + num2
+        elif operation == 'subtract':
+            return num1 - num2
+        elif operation == 'multiply':
+            return num1 * num2
+        elif operation == 'divide':
+            if num2 == 0:
+              return "Error: Division by zero"  # Handling division by zero
+            return num1 / num2
+        else:
+            return "Error: Invalid operation"  # Handling invalid operations
+        
