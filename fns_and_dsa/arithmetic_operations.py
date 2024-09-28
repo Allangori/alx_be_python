af386b2add299b1303eacc83fd553c5a14f98be3
Allan Gori <ogollogori@gmail.com>
2024-09-28 08:49:42 +0300

    new updates on arithmetic operations file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,21 @@
-def perform_operation ( num1, num2, operation):
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"  # Handling division by zero
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"  # Handling invalid operations
+
+
+
+
+#def perform_operation ( num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
