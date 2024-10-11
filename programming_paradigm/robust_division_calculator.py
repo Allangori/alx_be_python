e77d7412efbd6ef01d94a6ec47b5ab7ea3d55c3b
Allan Gori <ogollogori@gmail.com>
2024-10-11 09:19:29 +0300

    minor spelling update

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -15,5 +15,5 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
 
     except ValueError:
-        return "Error: Please enter numberic values only."
+        return "Error: Please enter numeric values only."
 
