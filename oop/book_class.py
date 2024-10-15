64137fc9121bf01dce369805a2075a9638a56426
Allan Gori <ogollogori@gmail.com>
2024-10-15 07:55:19 +0300

    new updates on the book_class.py file

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,28 @@
+# book_class.py
+
 class Book:
     def __init__(self, title, author, year):
+        """
+        Constructor to initialize the book's title, author, and publication year.
+        """
         self.title = title
         self.author = author
         self.year = year
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
-        
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+
     def __del__(self):
-        print(f"Deleting {self.title}")
+        """
+        Destructor to print a message when the book instance is deleted.
+        """
+        print(f"Deleting '{self.title}'")
 
+    def __str__(self):
+        """
+        String representation of the book for users.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-book1 = Book('1984', 'George Orwell', 1949)
-#print(book1) unofficial
-#print(repr(book1)) official
-del book1
+    def __repr__(self):
+        """
+        Official string representation of the book that can recreate the instance.
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
